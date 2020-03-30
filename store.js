a2242d5e179bef3953d3ec14bcb6fb5383293ae1
jsc <jsanchezcriado@gmail.com>
2020-03-30 12:36:25 +0200

    seeing it in action

--- store.js
+++ store.js
@@ -14,7 +14,7 @@ function createStore (reducer) {
     const subscribe = (listener) => {
         listeners.push(listener);
         return () => {
-            listener.filter(l => l != listener)
+            listeners = listeners.filter(l => l != listener)
         }
     };
 
@@ -49,5 +49,16 @@ const store = createStore(todosReducer);
 // Subscribe to changes in the state
 const unsubscribe = store.subscribe(() => console.log('state', store.getState()));
 
+const todoAction = {
+    type: 'ADD_TODO',
+    todo: 'First TODO'
+};
+
+// Dispatch Action
+store.dispatch(todoAction);
+
 // Unsubscribe
 unsubscribe();
+
+// Dispatch Action after unsubscribe
+store.dispatch(todoAction);
