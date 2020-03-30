a0b79846728cde9b3371e9ff1dd27c9b6fca8f6d
jsc <jsanchezcriado@gmail.com>
2020-03-30 20:23:21 +0200

    all in one single file

--- addTodoReducer.js
+++
@@ -1,7 +0,0 @@
-export function todosReducer(state=[], action) {
-    if (action.type === 'ADD_TODO') {
-        return [...state, action.todo]
-    }
-
-    return state;
-}
--- index.js
+++ index.js
@@ -1,15 +1,61 @@
-import { createStore } from "./store";
-import { todosReducer } from "./todosReducer";
+// Library Code
+function createStore (reducer) {
+    // The store should have four parts
+    // 1. The state
+    // 2. Get the state.
+    // 3. Listen to changes on the state.
+    // 4. Update the state
+  
+    let state;
+    let listeners = [];
+  
+    const getState = () => state;
 
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l != listener)
+        }
+    };
+
+    const dispatch = (action) => {
+        // update the state
+        state = reducer(state, action);
+
+        // nofify the change in the state to the listeners
+        listeners.forEach(listener => listener());
+    }
+  
+    return {
+        getState,
+        subscribe,
+        dispatch
+    }
+} 
+
+// Reducer
+function todo(state=[], action) {
+    if (action.type === 'ADD_TODO') {
+        return [...state, action.todo]
+    }
+
+    return state;
+}
+
+// CODE
 // Create store
-const store = createStore(todosReducer);
+const store = createStore(todo);
 
 // Subscribe to changes in the state
 const unsubscribe = store.subscribe(() => console.log('state', store.getState()));
 
 const todoAction = {
     type: 'ADD_TODO',
-    todo: 'First TODO'
+    todo: {
+        id: 0,
+        name: 'Eat healthy',
+        completed: true
+    }
 };
 
 // Dispatch Action
