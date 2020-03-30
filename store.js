9f7ac34f0387ad8834d8640540ae9bbe197cc75d
jsc <jsanchezcriado@gmail.com>
2020-03-30 12:13:08 +0200

    The reducer function

--- store.js
+++ store.js
@@ -1,3 +1,11 @@
+function todosReducer(state=[], action) {
+    if (action.type === 'ADD_TODO') {
+        return [...state, action.todo]
+    }
+
+    return state;
+}
+
 function createStore () {
     // The store should have four parts
     // 1. The state
